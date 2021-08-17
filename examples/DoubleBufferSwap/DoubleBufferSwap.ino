32696d1e543017420b24cc31e8cc43867a5f667f
mrfaptastic <12006953+mrfaptastic@users.noreply.github.com>
2021-08-17 11:47:46 +0100

    Update DoubleBufferSwap.ino

--- examples/DoubleBufferSwap/DoubleBufferSwap.ino
+++ examples/DoubleBufferSwap/DoubleBufferSwap.ino
@@ -1,16 +1,5 @@
 #include <ESP32-HUB75-MatrixPanel-I2S-DMA.h>
 
-
-/* 
- * Below is an is the 'legacy' way of initialising the MatrixPanel_I2S_DMA class.
- * i.e. MATRIX_WIDTH and MATRIX_HEIGHT are modified by compile-time directives.
- * By default the library assumes a single 64x32 pixel panel is connected.
- *
- * Refer to the example '2_PatternPlasma' on the new / correct way to setup this library
- * for different resolutions / panel chain lengths within the sketch 'setup()'.
- * 
- */
-
 MatrixPanel_I2S_DMA *display = nullptr;
 
 const byte row0 = 2+0*10;
