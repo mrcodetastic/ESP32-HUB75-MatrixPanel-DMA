97308637b4371c153a46d5774f7332055ae5d766
mrfaptastic <12006953+mrfaptastic@users.noreply.github.com>
2021-08-17 10:46:43 +0100

    Delete ESP32-RGB64x32MatrixPanel-I2S-DMA.h
    Remove legacy

--- ESP32-RGB64x32MatrixPanel-I2S-DMA.h
+++
@@ -1,4 +0,0 @@
-/* For backwards compatibility only */
-#pragma message "Please update your include to use ESP32-HUB75-MatrixPanel-I2S-DMA.h instead!"
-#include "ESP32-HUB75-MatrixPanel-I2S-DMA.h"
-typedef MatrixPanel_I2S_DMA RGB64x32MatrixPanel_I2S_DMA; // for backwards compatibility to old class name
