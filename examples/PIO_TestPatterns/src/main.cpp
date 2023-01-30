2c86ceee73a45193e7c31c1f6d6558c77c95c085
Elliot Matson <ematson5897@gmail.com>
2023-01-29 21:25:24 -0600

    does CI work now?

--- examples/PIO_TestPatterns/src/main.cpp
+++ examples/PIO_TestPatterns/src/main.cpp
@@ -1,14 +1,13 @@
 // How to use this library with a FM6126 panel, thanks goes to:
 // https://github.com/hzeller/rpi-rgb-led-matrix/issues/746
 
-
-// IDF
+//#ifdef IDF_BUILD
 #include <stdio.h>
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
 #include <driver/gpio.h>
 #include "sdkconfig.h"
-// IDF
+//#endif
 
 #include <Arduino.h>
 #include "xtensa/core-macros.h"
