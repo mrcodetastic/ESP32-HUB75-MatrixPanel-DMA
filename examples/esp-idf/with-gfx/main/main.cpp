993560d78935380ae2f37bdead24785b3585ee0f
Avi <avibrender@gmail.com>
2023-05-11 20:49:20 -0400

    Add Github Actions tests for esp-idf

---
+++ examples/esp-idf/with-gfx/main/main.cpp
@@ -0,0 +1,17 @@
+#include "ESP32-HUB75-MatrixPanel-I2S-DMA.h"
+
+#define WIDTH 64
+#define HEIGHT 64
+#define CHAIN 1
+
+MatrixPanel_I2S_DMA *dma_display = nullptr;
+
+extern "C" void app_main() {
+  HUB75_I2S_CFG mxconfig(WIDTH, HEIGHT, CHAIN);
+
+  dma_display = new MatrixPanel_I2S_DMA(mxconfig);
+  dma_display->begin();
+  dma_display->setBrightness8(80);
+  dma_display->clearScreen();
+  dma_display->println("Test message");
+}
