2f7de5b01c3d04e1773b8538175a33330efc097c
mrcodetastic <12006953+mrcodetastic@users.noreply.github.com>
2024-07-22 00:31:36 +0100

    Update gdma_lcd_parallel16.cpp

--- src/platforms/esp32s3/gdma_lcd_parallel16.cpp
+++ src/platforms/esp32s3/gdma_lcd_parallel16.cpp
@@ -133,7 +133,7 @@
     {
 
       auto  freq     = (_cfg.bus_freq);
-      auto  _div_num = 20; // 8Mhzhz 
+      auto  _div_num = 20; // 8Mhz 
       if (freq < 20000000L) {
             _div_num = 10; // 16Mhz
       } else {
